99b993d45dff9939e98cfc1d5b1ed0f9bbcc9ab1
jtbuch <jb4625@columbia.edu>
2025-04-02 12:14:16 -0400

    improved rb sims

--- shitty_bird/src/shitty_bird/core/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Core functionality for ShittyBird."""
-
-from .simulation import run_simulation
-from . import lbm
-
-__all__ = ["simulation", "run_simulation", "lbm"]
--- shitty_bird/src/shitty_bird/simulations/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Simulation implementations for ShittyBird."""
-
-from .rayleigh_benard import rayleigh_benard
-
-__all__ = ["rayleigh_benard"]
