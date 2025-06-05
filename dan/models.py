d9f4d40e6db2528297afb5dcb69b59cdbb0a9854
Ivan Grahek <grahek.i@gmail.com>
2025-06-05 14:30:46 -0700

    adding sensor/actuator noise and cleanup

--- dan/models.py
+++ dan/models.py
@@ -273,60 +273,39 @@ class MPCShittyBird:
         return reward, next_obs
 
     def mujoco_policy(self, env, obs):
-
-        # Save the current state of the environment using qpos and qvel
-        saved_qpos = copy.deepcopy(env.unwrapped.data.qpos)
-        saved_qvel = copy.deepcopy(env.unwrapped.data.qvel)
+        if self.model_type == 'mujoco':
+            saved_qpos = copy.deepcopy(env.unwrapped.data.qpos)
+            saved_qvel = copy.deepcopy(env.unwrapped.data.qvel)
+        else:
+            initial_obs = obs.copy()
 
         cumulative_reward = np.zeros(self.planning_width)
         actions = np.zeros([self.planning_width, self.n_planning, *env.action_space.shape])
 
         for i_trajectory in range(self.planning_width):
-            self.restore_state(env, saved_qpos, saved_qvel)
+            if self.model_type == 'mujoco':
+                self.restore_state(env, saved_qpos, saved_qvel)
+                current_obs = obs.copy()
+            else:
+                current_obs = initial_obs.copy()
 
             for step in range(self.n_planning):
-                # get random action
-                # action = env.action_space.sample()
-                # action = np.array([(np.random.rand()-0.5)*3])
-                # action = (np.random.rand(*env.action_space.shape) - 0.5) * 3
-
-                # Extract lower and upper bounds from the action space
-                low = env.action_space.low
-                high = env.action_space.high
-
-                # Generate a random action uniformly within [low, high]
-                action = np.random.uniform(low=low, high=high, size=env.action_space.shape)
-            
-                # # Perform the step
-                # obs_, reward, done, truncated, info = env.step(action)
-
-                # # Make the rewards be discrete or continuous
-                # if self.reward_type == 'discrete':
-                #      reward = reward
-                # elif self.reward_type == 'continuous':
-                #     reward = -np.abs(obs_[1])
-
-                # Perform the step and get the reward
-                reward, obs = self.take_step(env, obs, action)
-                # Print reward
-                # print(f"Reward: {reward}")
+                # action = np.random.uniform(low=env.action_space.low, high=env.action_space.high)
+                action = (np.random.rand(*env.action_space.shape) - 0.5) * 3
+                reward, next_obs = self.take_step(env, current_obs, action)
 
                 cumulative_reward[i_trajectory] += reward
                 actions[i_trajectory, step] = action
+                current_obs = next_obs
 
         self.restore_state(env, saved_qpos, saved_qvel)
 
-        #print(f"State: {obs_}")
-        #print(f"Cumulative reward: {cumulative_reward}")
-        #print(f"Best trajectory: {actions[i_trajectory, :]}")
-
-
         best_trajectory = np.argmax(cumulative_reward)
-        best_action = actions[best_trajectory, 0]
+        best_action_trajectory = actions[best_trajectory, :]
 
         
         # Return the best policy from this state forward
-        return best_action
+        return best_action_trajectory
     
 
     def get_forward_policy(self, state):
