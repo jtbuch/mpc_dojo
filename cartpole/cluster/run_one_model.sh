322fc58b95e629e02b5d65ce23b40d0c530547ff
Ivan Grahek <grahek.i@gmail.com>
2025-06-20 17:13:21 -0700

    Update run_one_model.sh

--- cartpole/cluster/run_one_model.sh
+++ cartpole/cluster/run_one_model.sh
@@ -2,7 +2,7 @@
 
 # Activate the desired conda environment
 module load anaconda
-source activate bird
+source activate shitty_bird_env
 
 # Run the Python script with the provided parameters
 python ../mpc/run.py "$1" "$2"
