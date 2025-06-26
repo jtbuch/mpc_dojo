053a4e8a97698ca5823c14082ac74f7c22e2c631
Ivan Grahek <grahek.i@gmail.com>
2025-06-26 17:23:54 +0200

    change sim params

--- cartpole/cluster/run_all_models.sh
+++ cartpole/cluster/run_all_models.sh
@@ -11,12 +11,12 @@
 #SBATCH -o log/R-%x.%j.out
 
 # Array of intervals and horizons
-declare -a intervals=(1 2 3)
+declare -a intervals=(1 2)
 declare -a horizons=(1 10 20 30 40 50 60 70 80 90 100) #1 20 40 60 80 100
-declare -a obs_noises=(0.0)
-declare -a action_noises=(0.0 0.1)
-declare -a obs_mus=(0.0 0.1)
-declare -a action_mus=(0.0)
+declare -a obs_noises=(0.0 0.4 0.8)
+declare -a action_noises=(0.0 0.1 0.2)
+declare -a obs_mus=(0.0 0.4)
+declare -a action_mus=(0.0 0.2)
 
 # Loop over all parameters
 for interval in "${intervals[@]}"; do
--- cartpole/mpc/run.py
+++ cartpole/mpc/run.py
@@ -27,8 +27,8 @@ config = {
     'obs_noise_sigma': np.array([obs_noise, obs_noise, obs_noise*0.1, obs_noise*0.1]),
     'act_noise_mu': np.array([act_mu]),
     'act_noise_sigma': np.array([act_noise]),
-    'n_episodes': 10,
-    'time_steps': 400,
+    'n_episodes': 20,
+    'time_steps': 1000,
     'action_cost': 0.2,
     'sampling_method': 'predictive',
     'recompute_intervals': [recompute_interval],  # Use the command line argument
