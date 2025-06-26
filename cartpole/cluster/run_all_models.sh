13df807872748468314aa704089d23bb33c24e21
Ivan Grahek <grahek.i@gmail.com>
2025-06-26 13:12:48 +0200

    Update run_all_models.sh

--- cartpole/cluster/run_all_models.sh
+++ cartpole/cluster/run_all_models.sh
@@ -12,11 +12,11 @@
 
 # Array of intervals and horizons
 declare -a intervals=(1 2 3)
-declare -a horizons=(1 20 40) #1 20 40 60 80 100
+declare -a horizons=(3 20) #1 20 40 60 80 100
 declare -a obs_noises=(0.0 0.4)
-declare -a action_noises=(0.0 0.4)
+declare -a action_noises=(0.0)
 declare -a obs_mus=(0.0 0.4)
-declare -a action_mus=(0.0 0.4)
+declare -a action_mus=(0.0)
 
 # Loop over all parameters
 for interval in "${intervals[@]}"; do
