b104cecd4000ce85249d9d1fda49085ec1754658
Ivan Grahek <grahek.i@gmail.com>
2025-07-11 13:58:09 +0200

    minor edits

--- cartpole/cluster/run_all_models.sh
+++ cartpole/cluster/run_all_models.sh
@@ -12,14 +12,14 @@
 
 # Array of intervals and horizons
 declare -a intervals=(1)
-declare -a horizons=(1 10 20) #30 40 50 60 70 80 90 100
+declare -a horizons=(1 10 20 30 40 50 60 70 80 90 100) #30 40 50 60 70 80 90 100
 declare -a obs_noises=(0.0 0.4)
 declare -a action_noises=(0.0 0.1)
 declare -a obs_mus=(0.0 0.2)
 declare -a action_mus=(0.0 0.1)
-declare -a control_models=("predictive" "rl")
+declare -a control_models=("predictive")
 declare -a values=("env" "rl")
-declare -a rl_models=("50k" "300k")
+declare -a rl_models=("50k")
 
 
 # Loop over all parameters
