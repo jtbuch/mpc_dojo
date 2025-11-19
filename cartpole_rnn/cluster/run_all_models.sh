b7c6b2fc277be06dc5cae447af9ebffec729c79b
Ivan Grahek <grahek.i@gmail.com>
2025-11-19 13:21:23 +0100

    new sims

--- cartpole_rnn/cluster/run_all_models.sh
+++ cartpole_rnn/cluster/run_all_models.sh
@@ -11,9 +11,8 @@
 #SBATCH -o log/R-%x.%j.out
 
 # Array of intervals and horizons
-declare -a length_ratios=(0.6 0.8 1.0 1.2 1.6)
-#declare -a recompute_intervals=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18)
-declare -a recompute_intervals=(1 2 3 4 5 6)
+declare -a length_ratios=(1.0 1.3 1.6 1.9)
+declare -a recompute_intervals=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18)
 declare -a wind_mus=(0.0 0.02 0.04 0.06)
 declare -a wind_sigmas=(0.0 0.05 0.1 0.15)
 declare -a world_model=('dynamics')
